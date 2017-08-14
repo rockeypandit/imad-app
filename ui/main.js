5eb99e7aca1342222d80135212d59002388ad0cf
Rockey <rockey.pandit98@gmail.com>
2017-08-15 00:04:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
-var button =document.getElementByid('counter');
+var button =document.getElementById('counter');
 var counter=0;
 
 
 button.onClick = function(){
   counter=counter+1;
-  var span = document.getElementByid('count');
+  var span = document.getElementById('count');
   span.innerHTML=counter.toString();
 };
