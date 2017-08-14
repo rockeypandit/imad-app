4464c9b5b04da11e6b359f180ef943c78f7b4ecd
Rockey <rockey.pandit98@gmail.com>
2017-08-14 22:51:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,6 @@ function moveRight(){
     img.style.marginLeft=marginLeft + `px`;
 }
 img.onclick = function (){
-    var interval =setInterval(moveRight,10);
+    var interval =setInterval(moveRight,100);
     //img.style.marginLeft = '100px';
 }
